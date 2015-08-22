a4d2f068fd7b4e96a16fc4d9c06c0f8e9eb8e602
unknown <r201-stu@AI241LAB0201P11.aipdcls.edu>
2015-08-21 20:30:18 -0700

    added camera switching script

--- SeptemberVMonster/Assets/Scripts/CORE/DataCore.cs
+++ SeptemberVMonster/Assets/Scripts/CORE/DataCore.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public static class DataCore {
 
+	public static List<Camera> listOfCameras = new List<Camera>();
 
 }
---
+++ SeptemberVMonster/Assets/Scripts/Camera/CameraTriggerSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTriggerSwitch : MonoBehaviour {
+	public Camera _cam = null;
+
+	// Use this for initialization
+	void Start () {
+		if(_cam != null) DataCore.listOfCameras.Add (_cam);
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.tag == "cameraFollowTarget") {
+			//Debug.Log("Ding! #");
+			foreach(Camera _camera in DataCore.listOfCameras) {
+				_camera.enabled = false;
+			}
+			_cam.enabled = true;
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
